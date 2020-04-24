4e82af2acf93370bd7faf9f2464a7a01eb367bdb
Roberto Díaz <robertocdiazsanchez@gmail.com>
2020-04-23 20:31:32 -0500

    Actualizada la plantilla Keys.ts

--- server/Keys.ts
+++ server/Keys.ts
@@ -1,4 +1,16 @@
+// TODO: Dar un valor a estas constantes.
 export class Keys {
-    // TODO: Dar un valor a estas constantes.
+    // Clave utilizada para encriptación.
     static SECRET_KEY: string = "";
+
+    // Credenciales de cuenta de correo del sistema. Tiene que ser GMAIL.
+    static GMAIL_EMAIL: string = "";
+    static GMAIL_PASSWORD: string = "";
+
+    // Datos para la conección con la base de datos en MySQL.
+    static DB_CONNECTION = {
+        host: '',
+        user: '',
+        password: ''
+    }
 }
