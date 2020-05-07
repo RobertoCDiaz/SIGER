614cf970f971a9342d127e01060f03d43f1bbf76
Roberto Díaz <robertocdiazsanchez@gmail.com>
2020-05-06 23:01:46 -0500

    Primer intento para generación de documentos

---
+++ server/docGenerator.ts
@@ -0,0 +1,29 @@
+var PizZip = require('pizzip');
+var Docxtemplater = require('docxtemplater');
+
+var fs = require('fs');
+var path = require('path');
+
+export const generate = (data, inputDir, outputDir) => new Promise(async (resolve, reject) => {
+    var content = fs
+    .readFileSync(/*path.resolve(__dirname, 'input.docx')*/inputDir, 'binary');
+
+    var zip = new PizZip(content);
+
+    var doc = new Docxtemplater();
+    doc.loadZip(zip);
+
+    doc.setData(data);
+
+    try {
+        doc.render()
+    } catch (error) {
+        reject(error);
+    }
+
+    var buf = doc.getZip().generate({type: 'nodebuffer'});
+
+    await fs.writeFileSync(/*path.resolve(__dirname, 'output.docx')*/ outputDir, buf);
+
+    resolve(buf);
+});
